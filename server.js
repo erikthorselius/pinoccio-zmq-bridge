5a4d976f6192df3009aa749ce977fe4a18bdbd6c
Erik Thorselius <erik.thorselius@gmail.com>
2016-01-31 19:29:32 +0100

    Change to connect instead

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 #!/usr/bin/env node
-
 var zmq = require('zmq'),
   sock = zmq.socket('pub'),
   server = require('pinoccio-server');
-
 
 var opts = {
   apiHost: process.env.PINOCCIO_API || 'pool.base.pinocc.io',
   apiPort: process.env.PINOCCIO_PORT || 22756
 };
 var zmqAddress = process.env.ZMQ_ADDRESS || 'tcp://127.0.0.1:3000';
-sock.bindSync(zmqAddress);
-console.log('Publisher bound to :' + zmqAddress);
+sock.connect(zmqAddress);
+console.log('Publisher bound to : ' + zmqAddress);
 server(opts, function(troop) {
   troop.on('data', function(ev) {
     console.log(troop.token + '>', JSON.stringify(ev));
