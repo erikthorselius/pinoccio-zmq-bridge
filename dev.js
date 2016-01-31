6bd04dc2b68c43a3b364dc260004484845066c1c
Erik Thorselius <erik.thorselius@gmail.com>
2016-01-31 19:13:34 +0100

    Now bridge data to zmq

---
+++ dev.js
@@ -0,0 +1,10 @@
+var zmq = require('zmq'),
+  sock = zmq.socket('sub');
+
+sock.connect('tcp://127.0.0.1:3000');
+sock.subscribe('sensor');
+console.log('Subscriber connected to port 3000');
+
+sock.on('message', function(topic, message) {
+  console.log('received a message related to:', topic, 'containing message:', message.toString('utf-8'));
+});
--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 #!/usr/bin/env node
 
-var server = require('pinoccio-server');
+var zmq = require('zmq'),
+  sock = zmq.socket('pub'),
+  server = require('pinoccio-server');
 
-// The server will try to forward everything to Pinoccio's
-// production servers. This can be nice because it gives you
-// event persistence, and you can still use HQ.
-// If you want to send it somewhere else, you can pass in
-// the environment variables.
+
 var opts = {
   apiHost: process.env.PINOCCIO_API || 'pool.base.pinocc.io',
   apiPort: process.env.PINOCCIO_PORT || 22756
 };
-
-server(opts, function(troop){    
-  
-  // Only one Troop will be connected to the server.
-  // `troop` is a stream you can read from, and write to.
-
-  // Log all the data streaming in from the Troop
-  troop.on('data', function(ev){
-    console.log(troop.token+'>', JSON.stringify(ev));
+var zmqAddress = process.env.ZMQ_ADDRESS || 'tcp://127.0.0.1:3000';
+sock.bindSync(zmqAddress);
+console.log('Publisher bound to :' + zmqAddress);
+server(opts, function(troop) {
+  troop.on('data', function(ev) {
+    console.log(troop.token + '>', JSON.stringify(ev));
+    sock.send(['sensor', JSON.stringify({
+      "sensor": "temperature",
+      "type":"pinoccio",
+      "data": ev
+    })]);
   });
 
-}).on('listening', function(){
+}).on('listening', function() {
   console.log('local pinoccio server listening on ', this.address());
 });
