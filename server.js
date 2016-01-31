e54384ecfea62b5ffde0015f2b641f10167ea341
Erik Thorselius <erik.thorselius@gmail.com>
2016-01-31 22:01:54 +0100

    Add more data to log message

--- server.js
+++ server.js
@@ -10,14 +10,27 @@ var opts = {
 var zmqAddress = process.env.ZMQ_ADDRESS || 'tcp://127.0.0.1:3000';
 sock.connect(zmqAddress);
 console.log('Publisher bound to : ' + zmqAddress);
+
+function createMsg(data) {
+  var msg = {
+    "sensor": "temperature",
+    "type": "pinoccio",
+    "data": data
+  }
+  switch (data.id) {
+    case 2:
+      msg.name = "Study"
+      break;
+    default:
+      msg.name = "Greenhouse"
+  }
+  return msg;
+}
+
 server(opts, function(troop) {
   troop.on('data', function(ev) {
     console.log(troop.token + '>', JSON.stringify(ev));
-    sock.send(['sensor', JSON.stringify({
-      "sensor": "temperature",
-      "type":"pinoccio",
-      "data": ev
-    })]);
+    sock.send(['sensor', JSON.stringify(createMsg(ev))]);
   });
 
 }).on('listening', function() {
